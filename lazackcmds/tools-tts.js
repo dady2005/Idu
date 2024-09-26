d6d49292e3ca54ea6cf939449002e175cd9b8303
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-09-26 23:22:57 +0300

    Update tools-tts.js

--- lazackcmds/tools-tts.js
+++ lazackcmds/tools-tts.js
@@ -1,47 +1,28 @@
-import gtts from 'node-gtts'
-import { readFileSync, unlinkSync } from 'fs'
-import { join } from 'path'
+//base by DGXeon
+//re-upload? recode? copy code? give credit ya :)
+//YouTube: @DGXeon
+//Instagram: unicorn_xeon13
+//Telegram: t.me/xeonbotinc
+//GitHub: @DGXeon
+//WhatsApp: +916909137213
+//want more free bot scripts? 
+//subscribe to my youtube channel: https://youtube.com/@DGXeon
 
-const defaultLang = 'en'
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  let lang = args[0]
-  let text = args.slice(1).join(' ')
-  if ((args[0] || '').length !== 2) {
-    lang = defaultLang
-    text = args.join(' ')
-  }
-  if (!text && m.quoted?.text) text = m.quoted.text
+const { join } = require('path');
+const gtts = require('node-gtts');
+const { readFileSync, unlinkSync } = require('fs');
 
-  let res
-  try {
-    res = await tts(text, lang)
-  } catch (e) {
-    m.reply(e + '')
-    text = args.join(' ')
-    if (!text) throw `📌 Example : \n${usedPrefix}${command} en hello world`
-    res = await tts(text, defaultLang)
-  } finally {
-    if (res) conn.sendFile(m.chat, res, 'tts.opus', null, m, true)
-  }
-}
-handler.help = ['tts <lang> <task>']
-handler.tags = ['tools']
-handler.command = ['tts', 'voz']
-
-export default handler
-
-function tts(text, lang = 'en-en') {
-  console.log(lang, text)
+function tts(text, lang = 'id') {
   return new Promise((resolve, reject) => {
     try {
       let tts = gtts(lang)
-      let filePath = join(global.__dirname(import.meta.url), '../tmp', 1 * new Date() + '.wav')
+      let filePath = join(__dirname, '..XeonMedia/trash', (1 * new Date) + '.wav')
       tts.save(filePath, text, () => {
         resolve(readFileSync(filePath))
         unlinkSync(filePath)
       })
-    } catch (e) {
-      reject(e)
-    }
+    } catch (e) { reject(e) }
   })
 }
+
+module.exports = { tts }

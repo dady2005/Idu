04f8c52a6f54cd3566a9eaf1777a24d7162dd52b
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-09-26 23:13:57 +0300

    Update Hello.js

--- lazackcmds/Hello.js
+++ lazackcmds/Hello.js
@@ -3,7 +3,7 @@ export async function all(m) {
   if (
     (m.mtype === 'hellomessage' ||
       m.text.startsWith('Hello') ||
-      m.text.startsWith('Hi') ||
+      m.text.startsWith('oya veap') ||
       m.text.startsWith('Mambo') ||
       m.text.startsWith('Oy') ||
       m.text.startsWith('Niaje') ||
@@ -18,9 +18,9 @@ export async function all(m) {
       },
       { quoted: m }*/
     ) {
-    this.sendButton(m.chat, `*WELCOME ITS ME JUST REPLYING*      
-    morning or evening @${m.sender.split('@')[0]} 
-    i may be offline or i may be slow to respond you but wait i will be back soon 😇
+    this.sendButton(m.chat, `*ASANTE KWA KUWASILIANA NA MICK*      
+    MORNING OR EVENING @${m.sender.split('@')[0]} 
+    KUWA NA SUBRA NITAKUJA HEWANI MDA SIO MREFU  😇
   `.trim(), igfg, null, [['OWNER HELP', '.grp'],['GET TEXT', '.repo']] , m, { mentions: [m.sender] })
     m.react('🤫')
   }

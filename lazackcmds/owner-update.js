ea1175d5059977fc7f1320695a48bd783c8c0b2d
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-10-11 22:19:30 +0300

    Update owner-update.js

--- lazackcmds/owner-update.js
+++ lazackcmds/owner-update.js
@@ -1,14 +1,16 @@
-import { execSync } from 'child_process'
-let handler = async (m, { conn, text }) => {
-await m.react('🕓')
-if (conn.user.jid == conn.user.jid) {
-let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m)
-await m.react('✅')
-}}
+import { spawn } from 'child_process'
+let handler = async (m, { conn, isROwner, text }) => {
+  if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
+  if (conn.user.jid == conn.user.jid) {
+    await m.reply('🔄 updating Bot...\n Wait a moment')
+    process.send('reset')
+  } else throw 'eh'
+}
+
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update']
+
 handler.rowner = true
 
 export default handler

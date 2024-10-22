724d4602293a0789292ada580b1f1ebf7692f1c1
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-10-22 13:22:28 +0300

    Update test.js

--- lazackcmds/test.js
+++ lazackcmds/test.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
-    let img = "https://avatars.githubusercontent.com/u/106463398?v=4"
-let info = `*XLICON BOT IS ACTIVE*`
+    let img = "https://i.imgur.com/O3AGz9U.jpeg"
+let info = `*DENZEL V2 BOT IS ACTIVE*`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
 }

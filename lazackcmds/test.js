3599177f82c4c4c85c7dbf6a70bc7c742f05fd9a
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-11-10 14:00:15 +0300

    Update test.js

--- lazackcmds/test.js
+++ lazackcmds/test.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import fetch from 'node-fetch'
 let handler  = async (m, { conn, usedPrefix: _p }) => {
     let img = "https://i.imgur.com/O3AGz9U.jpeg"
-let info = `*DENZEL V2 BOT IS ACTIVE*`
+let info = `*Dear he/she i am active *`
 await conn.reply(m.chat, info, m, { contextInfo: { mentionedJid: [m.sender],forwardingScore: 256,
       isForwarded: true, externalAdReply: { title: author, body: botname, sourceUrl: fgyt, thumbnail: await conn.getFile(img) }}})
 }

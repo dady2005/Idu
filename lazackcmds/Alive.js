26bb4bfaa4672c09f1fb6cd1452f7223e706f93c
Mickeymozy <157571260+Mickeymozy@users.noreply.github.com>
2024-11-10 13:43:41 +0300

    Update Alive.js

--- lazackcmds/Alive.js
+++ lazackcmds/Alive.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/Denzel-V2@main/jusorts/lazack.mp3'
+  var vn = 'https://cdn.jsdelivr.net/gh/Mickeymozy/Denzel-V2@main/jusorts/lazack.mp3'
   let url = 'https://github.com/Mickeymozy/Denzel-V2'
   let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
   let img = 'https://i.imgur.com/2rN3Xy7.jpeg'
